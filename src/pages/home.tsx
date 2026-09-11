e9bb6f3547b25c18495f0a8d7bfbd9bb51888009
execterminal <boba3ma@outlook.com>
2026-09-11 08:50:29 +0500

    normal  the main menu

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,18 +11,22 @@ const Home: React.FC = () => {
       <div className="flex justify-center items-center flex-col">
         <div className="border border-red-300 w-full h-30 flex justify-center items-center">
           <Input placeholder={t("home.inputPlaceholder")}></Input>
-          <Button className="rounded-r-2xl border-border border-l-0 h-17 w-25 bg-secondary cursor-pointer">
+          <Button className="rounded-r-2xl border-border border-l-0 h-17 w-25 bg-myred cursor-pointer">
             {t("home.inputAddButton")}
           </Button>
           {/* <button className="border border-l-2xl">Add</button> */}
         </div>
 
-        <div>
-          <Button className="border bg-foreground">Mark All Done</Button>
-          <Button className="border bg-foreground">Act Now, Simplify Life.☕</Button>
+        <div className="flex">
+          <Button className="bg-mygreen border-border border-r-0 border-b-0 rounded-tl-2xl h-10 w-40">
+            Mark All Done
+          </Button>
+          <Button className="font-bold hover:bg-#8cd4cb border-border border-b-0 rounded-tr-2xl bg-foreground hover:bg-myyellow1 h-10 w-127 flex justify-start p-5">
+            Act Now, Simplify Life.☕
+          </Button>
         </div>
 
-        <div className="border w-167 bg-foreground">
+        <div className="border w-167 bg-foreground p-10 flex flex-col gap-4 pb-20">
           <p>Add Your First To-Do Item! 📝</p>
           <p>Usage Tips 💡:</p>
           <p>✔️ Press Enter to submit actions.</p>
@@ -33,7 +37,7 @@ const Home: React.FC = () => {
           <p>📝 Supports data download and import. </p>
         </div>
 
-        <div className="border bg-foreground w-167">3</div>
+        <div className="border border-t-0 rounded-b-2xl bg-foreground w-167 h-10"></div>
       </div>
     </div>
   );
